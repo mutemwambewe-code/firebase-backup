b2dd5d9fb3808338c11cda193510b427d5cd66b1
MUTEMWA MBEWE <mutemwambewe@gmail.com>
2025-10-12 11:34:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1238223014-799ca",
+  "appId": "1:337337233505:web:7d671917d508188c703a20",
+  "apiKey": "AIzaSyD9KSKH_YQfu92ksvKR9FgYGEZbVTJkFRY",
+  "authDomain": "studio-1238223014-799ca.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "337337233505"
+};
